15f54ed998ca9b10de07ff25e17086c8824ff37c
Ahnzi <42331481+ahnzi91@users.noreply.github.com>
2025-02-25 16:33:23 +0900

    👔enhancement: 관리자 템플릿 대시보드 업데이트

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,7 +1,7 @@
 const projects = [
   {
     id: "good-store", // 문자열 ID 사용
-    image: "/assets/images/i-land.png",
+    image: "/assets/images/i-land-1.png",
     title: "굿즈 스토어",
     techStack: [
       "React",
@@ -21,8 +21,8 @@ const projects = [
     title: "관리자 템플릿",
     techStack: ["HTML", "TailwindCSS", "JavaScript", "Github Pages"],
     description: "반응형 관리자 대시보드 템플릿을 만들었습니다.",
-    githubLink: "#",
-    liveDemoLink: "#",
+    githubLink: "https://github.com/ahnzi91/admin-template-tailwindcss",
+    liveDemoLink: "https://ahnzi91.github.io/admin-template-tailwindcss/",
   },
 ];
 
